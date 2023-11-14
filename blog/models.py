88df25fb74ab0081102d5e4c42af7a3936dccdf4
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-14 13:54:00 -0600

    add post model and tinymce HTMLField

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-# import uuid
-# from django.contrib.auth.models import User
+import uuid
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         abstract = True
@@ -17,3 +18,17 @@ class Category(BaseModel):
     def __str__(self):
         return self.name
         
+class Post(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post', null=True)
+    post_id = models.CharField(max_length=255, null=False, default=uuid.uuid4().__str__())
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='post', null=True)
+    description = models.TextField(null=True)
+    content = HTMLField()
+    title = models.CharField(max_length=300, null=True)
+    slug = models.CharField(max_length=255, null=False)
+    featured = models.BooleanField(default=False)
+    likes = models.ManyToManyField(User, related_name='likes')
+
+    def __str__(self):
+        return self.name
