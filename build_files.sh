efaa2045862a7b1a468765030de8efe3dd58beb7
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-11-15 15:54:47 -0600

    for deploy

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,4 @@
  echo "BUILD START"
- python3.9 -m pip install --upgrade pip
- python3.9 -m pip uninstall crispy-bootstrap5
- python3.9 -m pip uninstall django
  python3.9 -m pip install -r requirements.txt
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
